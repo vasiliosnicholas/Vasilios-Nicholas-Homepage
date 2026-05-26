43ff0cd3ce59ff14c5a8d13ef4527add1aa707a1
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-05-26 00:58:29 -0400

    implemented background image gallery with image description!

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 const backgroundColorOne = "bg-white";
 const flashTime = 2000;
+const backgroundTime = 10000;
 const simpleFlashElements = document.querySelectorAll(".flash");
 const sequenceFlashElements = document.querySelectorAll(".flash-seq");
 const displayButton = document.querySelector("#display-toggle");
+const imgDescription = document.querySelector(".img-description");
 
 async function flashElement(element, color) {
   element.classList.toggle(color);
@@ -33,17 +35,41 @@ async function flashInterval() {
   count = count % 2;
 }
 
+async function loadBackgrounds() {
+  const data = await fetch("./images/backgrounds.json");
+  return await data.json();
+}
+
+async function changeBackground(image) {
+  imgDescription.innerHTML =
+    `<strong> Image Description:</strong> ` + image.description;
+  document.body.style.setProperty("--bg-image", `url("${image.path}")`);
+}
+
+changeBackground((await loadBackgrounds())[0]);
+
+//simple flash
 setInterval(async () => {
   for (const element of simpleFlashElements) {
     await flashElement(element, backgroundColorOne);
   }
 }, flashTime);
 
+//sequential flash
 if (sequenceFlashElements.length > 0) {
   setInterval(async () => {
     await flashInterval();
   }, flashTime);
 }
-//document.body.style.backgroundImage = "url('./images/profile_photo.png')";
 
+let bIndex = 0;
+setInterval(async () => {
+  const backgrounds = await loadBackgrounds(); //TODO: Find a better solution than reloading this everytime
+  if (backgrounds != undefined && backgrounds.length > 0) {
+    await changeBackground(backgrounds[bIndex++]);
+    bIndex = bIndex % backgrounds.length;
+  }
+}, backgroundTime);
 displayButton.addEventListener("click", displayToggle);
+
+imgDescription;
